bf8e25ba68cc695c2076247534ebede0e7617875
Xsozm <hazem29778865@gmail.com>
2019-07-14 10:06:11 +0200

    addRoles

--- middlewares/AuthMiddleWare.js
+++ middlewares/AuthMiddleWare.js
@@ -31,101 +31,120 @@ const AuthMiddleWare = async (req, res, next) => {
 
         next();
       });
-      console.log('exists');
     } else {
       res.json({
         status: 'error',
-        message: 'Token not provided please login first !',
+        message: 'Token not provided or Invalid please login first !',
         data: null,
       });
-      console.log("doesn't exist");
     }
   });
 };
 
-const AuthMiddleWareIsBloger = (req, res, next) => {
-  if (!req.headers['x-access-token']) {
+const AuthMiddleWareIsBloger = async (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
+  if (!token) {
     res.json({
       status: 'error',
       message: 'Token not provided please login first !',
       data: null,
     });
   }
+  redisClient.exists(token, (err, reply) => {
+    if (reply === 1) {
+      jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+        if (error) {
+          res.json({
+            status: 'error',
+            message: 'Token not provided please login first !',
+            data: null,
+          });
+        }
+        User.findOne({ _id: decoded.user_id }, (error, existinguser) => {
+          if (error) {
+            console.log(error);
+          } else if (!existinguser) {
+            res.json({
+              status: 'Failed',
+              message: 'User Does Not Exist !!!',
+              data: null,
+            });
+          } else if (existinguser.role !== 'blogger') {
+            res.json({
+              status: 'Failed',
+              message: 'Un Authorized Action !!!',
+              data: null,
+            });
+          } else {
+            // add user id to request
+            req.body.userId = decoded.user_id;
+            req.body.token = token;
 
-  jwtr.verify(req.headers['x-access-token'], process.env.SECRET_KEY, (err, decoded) => {
-    if (err) {
-      res.json({
-        status: 'error',
-        message: 'Your token is not valid ,Login again !',
-        data: null,
+            next();
+          }
+        });
       });
     } else {
-      User.findOne({ _id: decoded.user_id }, (error, existinguser) => {
-        if (error) {
-          console.log(error);
-        } else if (!existinguser) {
-          res.json({
-            status: 'Failed',
-            message: 'User Does Not Exist !!!',
-            data: null,
-          });
-        } else if (existinguser.role !== 'blogger') {
-          res.json({
-            status: 'Failed',
-            message: 'Un Authorized Action !!!',
-            data: null,
-          });
-        } else {
-          // add user id to request
-          req.body.userId = decoded.user_id;
-
-          next();
-        }
+      res.json({
+        status: 'error',
+        message: 'Token not provided or Invalid please login first !',
+        data: null,
       });
     }
   });
 };
 
-const AuthMiddleWareIsAdmin = (req, res, next) => {
-  if (!req.headers['x-access-token']) {
+const AuthMiddleWareIsAdmin = async (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
+  if (!token) {
     res.json({
       status: 'error',
       message: 'Token not provided please login first !',
       data: null,
     });
   }
+  redisClient.exists(token, (err, reply) => {
+    if (reply === 1) {
+      jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+        if (error) {
+          res.json({
+            status: 'error',
+            message: 'Token not provided please login first !',
+            data: null,
+          });
+        }
+        User.findOne({ _id: decoded.user_id }, (error, existinguser) => {
+          if (error) {
+            console.log(error);
+          } else if (!existinguser) {
+            res.json({
+              status: 'Failed',
+              message: 'User Does Not Exist !!!',
+              data: null,
+            });
+          } else if (existinguser.role !== 'admin') {
+            res.json({
+              status: 'Failed',
+              message: 'Un Authorized Action !!!',
+              data: null,
+            });
+          } else {
+            // add user id to request
+            req.body.userId = decoded.user_id;
+            req.body.token = token;
 
-  jwtr.verify(req.headers['x-access-token'], process.env.SECRET_KEY, (err, decoded) => {
-    if (err) {
-      res.json({
-        status: 'error',
-        message: 'Your token is not valid ,Login again !',
-        data: null,
+            next();
+          }
+        });
       });
     } else {
-      User.findOne({ _id: decoded.user_id }, (error, existinguser) => {
-        if (error) {
-          console.log(error);
-        } else if (!existinguser) {
-          res.json({
-            status: 'Failed',
-            message: 'User Does Not Exist !!!',
-            data: null,
-          });
-        } else if (existinguser.role !== 'admin') {
-          res.json({
-            status: 'Failed',
-            message: 'Un Authorized Action !!!',
-            data: null,
-          });
-        } else {
-          // add user id to request
-          req.body.userId = decoded.user_id;
-
-          next();
-        }
+      res.json({
+        status: 'error',
+        message: 'Token not provided or Invalid please login first !',
+        data: null,
       });
     }
   });
 };
+
 export { AuthMiddleWare, AuthMiddleWareIsAdmin, AuthMiddleWareIsBloger };
